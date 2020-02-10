80a07dcd4a4a17f361d49d3cc2ef7da196becfad
Matt Difford <matt@mrsavvy.co.uk>
2020-02-10 14:54:33 +0000

    Initial

--- js/bundle.js
+++ js/bundle.js
@@ -1,6 +1,7 @@
 ---
 ---
 
+{% include_relative  src/jquery.min.js %}
 {% include_relative  src/modernizr.min.js %}
 {% include_relative  src/script.js %}
 {% include_relative  src/lazysizes.min.js %}
