09d56081769d4c51d26a07d9472e0063333ee582
Matt Difford <matt@mrsavvy.co.uk>
2020-11-25 11:33:07 +0000

    Lead routing etc

--- js/bundle.js
+++ js/bundle.js
@@ -5,7 +5,6 @@
 {% include_relative  src/modernizr.min.js %}
 {% include_relative  src/lazysizes.min.js %}
 {% include_relative  src/anime.min.js %}
-{% include_relative  src/parsley.min.js %}
 {% include_relative  src/oridomi.min.js %}
 {% include_relative  src/fullpagescroll.min.js %}
 {% include_relative  src/script.js %}
